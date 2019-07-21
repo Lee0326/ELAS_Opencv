c3d08b7b66016d341340234cedc7dcda7645aedd
Lee0326 <lcyfly1@163.com>
2019-07-21 23:49:33 +0800

    test:Test support match

---
+++ orb_elas/build/CMakeFiles/orb_elas.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orb_elas.dir/src/main.cpp.o"
+  "CMakeFiles/orb_elas.dir/src/elas.cpp.o"
+  "CMakeFiles/orb_elas.dir/src/orb_extractor.cpp.o"
+  "CMakeFiles/orb_elas.dir/src/descriptor.cpp.o"
+  "orb_elas.pdb"
+  "orb_elas"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orb_elas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ orig_elas/build/CMakeFiles/test_ori_elas.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lee/ELAS_Opencv/orig_elas/src/descriptor.cpp" "/home/lee/ELAS_Opencv/orig_elas/build/CMakeFiles/test_ori_elas.dir/src/descriptor.cpp.o"
+  "/home/lee/ELAS_Opencv/orig_elas/src/elas.cpp" "/home/lee/ELAS_Opencv/orig_elas/build/CMakeFiles/test_ori_elas.dir/src/elas.cpp.o"
+  "/home/lee/ELAS_Opencv/orig_elas/src/filter.cpp" "/home/lee/ELAS_Opencv/orig_elas/build/CMakeFiles/test_ori_elas.dir/src/filter.cpp.o"
+  "/home/lee/ELAS_Opencv/orig_elas/src/main.cpp" "/home/lee/ELAS_Opencv/orig_elas/build/CMakeFiles/test_ori_elas.dir/src/main.cpp.o"
+  "/home/lee/ELAS_Opencv/orig_elas/src/matrix.cpp" "/home/lee/ELAS_Opencv/orig_elas/build/CMakeFiles/test_ori_elas.dir/src/matrix.cpp.o"
+  "/home/lee/ELAS_Opencv/orig_elas/src/triangle.cpp" "/home/lee/ELAS_Opencv/orig_elas/build/CMakeFiles/test_ori_elas.dir/src/triangle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../..."
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
